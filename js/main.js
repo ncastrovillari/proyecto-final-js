b2264cf87e5f3fdb75f4af6163e0a60c95455edf
nicolas castrovillari <ncastrovillari@gmail.com>
2022-07-19 23:56:57 -0300

    1era entrega

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Calculadora que devuelve el total de pesos a pagar por gastos realizados en dólares con los impuestos incluidos //
+
+function gastos(valorDolar, gastoDolar) {
+    return totalPesos = ((valorDolar * gastoDolar))
+}
+let gastoPesos = gastos(parseFloat(prompt("Ingrese cotización del dólar oficial"))
+    , parseFloat(prompt("Ingrese sus gastos en dólares")))
+document.write("Gasto en pesos es: " + gastoPesos)
+
+let impuestoPais = gastoPesos * 0.30
+document.write("<br>" + "<br>" + "Impuesto PAIS del 30%: " + "$" + impuestoPais)
+
+let retencion = gastoPesos * 0.45
+document.write("<br>" + "<br>" + "Retención a cuenta de Ganancias del 45%: " + "$" + retencion)
+
+let totalPagar = gastoPesos + impuestoPais + retencion
+document.write("<br>" + "<br>" + "Total a pagar en pesos: " + totalPagar)
